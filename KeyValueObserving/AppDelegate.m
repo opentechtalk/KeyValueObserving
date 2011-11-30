f7940e28dbfa46b1a90ceba28b805d05e34ec76a
OpenTechTalk <opentechtalk@gmail.com>
2011-11-30 13:52:37 +0100

    Implemented Notifications for the button and the member text.
    + Added IBOutlet for button.
    + Connected IBOutlet with button in Interface Builder.
    + Registered for notifications in awakeFromNib.
    + Sending the notification in setter of member text.
    + Updated screenshot.

--- KeyValueObserving/AppDelegate.m
+++ KeyValueObserving/AppDelegate.m
@@ -8,19 +8,44 @@
 
 #import "AppDelegate.h"
 
+
+NSString* const kTextChangedNotification = @"TextChanged";
+
+
 @implementation AppDelegate
 
 @synthesize window = _window;
 @synthesize text = m_text;
+@synthesize button = m_button;
 
+- (void)setText:(NSString*)text {
+	m_text = text;
+	
+	// Sending the notification.
+	NSNotificationCenter* notificationCenter = [NSNotificationCenter defaultCenter];
+	[notificationCenter postNotificationName:kTextChangedNotification object:self];
+}
 
 - (void)dealloc {
     [super dealloc];
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification*)notification {
+	self.text = @"Notifications";
 }
 
+- (void)awakeFromNib {
+	
+	assert(m_button);
+	
+	// Register for notfications.
+	NSNotificationCenter* notificationCenter = [NSNotificationCenter defaultCenter];
+	[notificationCenter addObserver:self selector:@selector(textChanged:) name:kTextChangedNotification object:self];
+}
+
+- (void)textChanged:(NSNotification*)notification {
+	self.button.title = self.text;
+}
 
 
 @end
